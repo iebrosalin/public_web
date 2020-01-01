abcda6de6da9dd589ce5a0a0212031b072622aec
iebrosalin <iebrosalin@gmail.com>
2020-01-01 12:52:27 +0700

    replace strings replace layout from back to front

--- index.js
+++ index.js
@@ -21,31 +21,12 @@ io.on('connection', function (socket) {
         console.log('data form step 1: ');
         console.log(msg);
         var res = sleep(2000);
-        socket.emit('form step 1', '<ul class=" mb-5 nav nav-pills" nav >\n' +
-            '                <li class="nav-item">\n' +
-            '                    <a class="nav-link">Step 1</a>\n' +
-            '                </li>\n' +
-            '                <li class="nav-item">\n' +
-            '                    <a class="nav-link active">Step 2</a>\n' +
-            '                </li>\n' +
-            '\n' +
-            '            </ul>\n' +
-            '                <form step2 method="post">\n' +
-            '                    <fieldset>\n' +
-            '                        <legend>Enter verify code</legend>\n' +
-            '\n' +
-            '                        <div class="form-group">\n' +
-            '                            <label >Код</label>\n' +
-            '                            <input type="text" class="form-control" name="code" placeholder="Код">\n' +
-            '                        </div>\n' +
-            '                        <button type="submit" class="btn btn-primary">Submit</button>\n' +
-            '                    </fieldset>\n' +
-            '                </form>');
+        socket.emit('form step 1', 'ok');
 
     });
     socket.on('form step 2', function (msg) {
         console.log('data form step 2: ');
         console.log(msg);
-        socket.emit('form step 2', '<h5>Success</h5>');
+        socket.emit('form step 2', 'ok');
     });
 });
