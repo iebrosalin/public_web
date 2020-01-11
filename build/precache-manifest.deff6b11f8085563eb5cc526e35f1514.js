398a7866a8083a29723cf5c84ee838622737b432
IEBrosalin <iebrosalin@gmail.com>
2020-01-12 00:53:16 +0700

    update version bootstrap change theme to dark update CRA

---
+++ build/precache-manifest.deff6b11f8085563eb5cc526e35f1514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6797895ce5dfc96b68dc5d366406611e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bbd76086ff1c3c5cef1",
+    "url": "/static/css/main.60129b46.chunk.css"
+  },
+  {
+    "revision": "07092eac384d610020be",
+    "url": "/static/js/2.4e95955d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4e95955d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bbd76086ff1c3c5cef1",
+    "url": "/static/js/main.a9c288f0.chunk.js"
+  },
+  {
+    "revision": "d508612cddbc4b6a5b0d",
+    "url": "/static/js/runtime-main.3948777d.js"
+  }
+]);
