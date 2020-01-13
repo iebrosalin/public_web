de1812e3bc4d5e19222cba46c09768b406325449
IEBrosalin <iebrosalin@gmail.com>
2020-01-14 00:56:27 +0700

    added description addded margin for overlay text

--- webpack.config.js
+++ webpack.config.js
@@ -3,104 +3,104 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env = {}) => {
 
-  const { mode = 'development' } = env;
+    const {mode = 'development'} = env;
 
-  const isProd = mode === 'production';
-  const isDev = mode === 'development';
+    const isProd = mode === 'production';
+    const isDev = mode === 'development';
 
-  const getStyleLoaders = () => {
-    return [
-      isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-      'css-loader'
-    ];
-  };
+    const getStyleLoaders = () => {
+        return [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
+        ];
+    };
 
-  const getPlugins = () => {
-    const plugins = [
-      new HtmlWebpackPlugin({
-        title: 'Sandbox build',
-        "template": 'src/index.html',
-        "buildTime": new Date().toISOString(),
-        'meta': {
-          'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no',
-          'theme-color': '#000000'
-        }
-      })
-    ];
+    const getPlugins = () => {
+        const plugins = [
+            new HtmlWebpackPlugin({
+                title: 'Sandbox build',
+                "template": 'src/index.html',
+                "buildTime": new Date().toISOString(),
+                'meta': {
+                    'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no',
+                    'theme-color': '#000000'
+                }
+            })
+        ];
 
-    if (isProd) {
-      plugins.push(new MiniCssExtractPlugin({
-            filename: 'main-[hash:8].css'
-          })
-      );
-    }
-
-    return plugins;
-  };
-
-  return {
-    mode: isProd ? 'production': isDev && 'development',
-
-    output: {
-      filename: isProd ? 'main-[hash:8].js' : undefined
-    },
-
-    module: {
-      rules: [
-
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-        },
-
-        // Loading images
-        {
-          test: /\.(png|jpg|jpeg|gif|ico)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                outputPath: 'images',
-                name: '[name]-[sha1:hash:7].[ext]'
-              }
-            }
-          ]
-        },
-
-        // Loading fonts
-        {
-          test: /\.(ttf|otf|eot|woff|woff2)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                outputPath: 'fonts',
-                name: '[name].[ext]'
-              }
-            }
-          ]
-        },
-
-        // Loading CSS
-        {
-          test: /\.(css)$/,
-          use: getStyleLoaders()
-        },
-
-        // Loading SASS/SCSS
-        {
-          test: /\.(s[ca]ss)$/,
-          use: [ ...getStyleLoaders(), 'sass-loader' ]
+        if (isProd) {
+            plugins.push(new MiniCssExtractPlugin({
+                    filename: 'main-[hash:8].css'
+                })
+            );
         }
 
-      ]
-    },
+        return plugins;
+    };
 
-    plugins: getPlugins(),
+    return {
+        mode: isProd ? 'production' : isDev && 'development',
 
-    devServer: {
-      open: true
-    }
-  };
+        output: {
+            filename: isProd ? 'main-[hash:8].js' : undefined
+        },
+
+        module: {
+            rules: [
+
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+
+                // Loading images
+                {
+                    test: /\.(png|jpg|jpeg|gif|ico)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'images',
+                                name: '[name]-[sha1:hash:7].[ext]'
+                            }
+                        }
+                    ]
+                },
+
+                // Loading fonts
+                {
+                    test: /\.(ttf|otf|eot|woff|woff2)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'fonts',
+                                name: '[name].[ext]'
+                            }
+                        }
+                    ]
+                },
+
+                // Loading CSS
+                {
+                    test: /\.(css)$/,
+                    use: getStyleLoaders()
+                },
+
+                // Loading SASS/SCSS
+                {
+                    test: /\.(s[ca]ss)$/,
+                    use: [...getStyleLoaders(), 'sass-loader']
+                }
+
+            ]
+        },
+
+        plugins: getPlugins(),
+
+        devServer: {
+            open: true
+        }
+    };
 };
