0cc942b6e06053ad5279d6ac8b76084bd173da4d
iebrosalin <iebrosalin@gmail.com>
2020-01-17 09:50:50 +0700

    change name project

--- www/views/layouts/header_admin.php
+++ www/views/layouts/header_admin.php
@@ -27,7 +27,7 @@ use Components\Helpers\Helpers;
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-        <a class="navbar-brand" href="">Cutted-admin panel</a>
+        <a class="navbar-brand" href="">Slim admin panel</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
