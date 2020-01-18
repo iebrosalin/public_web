f722a4dac6687570e2813840a6cdbeb646a5ad66
iebrosalin <iebrosalin@gmail.com>
2020-01-18 19:07:17 +0700

    init project added fabric method

--- www/config/db_params.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    'host' => getenv('DB_HOST'),
-    'dbname' => getenv('MYSQL_DATABASE'),
-    'user' => getenv('MYSQL_USER'),
-    'password' => getenv('MYSQL_PASSWORD'),
-];
