e817e5f7968e9a424b0916d0146fe6a2907d1328
IEBrosalin <iebrosalin@gmail.com>
2020-01-23 18:01:24 +0700

    20 lesson

---
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use App\Models\BlogCategory;
+use Illuminate\Http\Request;
+
+class CategoryController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $paginator = BlogCategory::paginate(15);
+
+        return view('blog.admin.categories.index', compact('paginator'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        dd(__METHOD__);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        dd(__METHOD__);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $item = BlogCategory::findOrFail($id);
+//        $item = BlogCategory::find($id);
+//        $item []= BlogCategory::findOrFail($id);
+//        $item []= BlogCategory::where('id','=' ,$id)->first();
+//        $item []= BlogCategory::where('id','=' ,$id)->get(); // возвращает array
+//        $item []= BlogCategory::where('id', $id)->first();
+//        dd($item);
+
+        $categoryList = BlogCategory::all();
+
+        return view( 'blog.admin.categories.edit',
+            compact('item', 'categoryList')
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = BlogCategory::find($id);
+        if (empty($item)) {
+            return back()
+                ->withErrors(['msg' => "Запись id=[{$id}] не найдена"])
+                ->withInput();
+        }
+
+        $data = $request->all();
+        $result = $item->fill($data)->save();
+
+        if($result){
+            return back()
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Ошибка сохранения'])
+                ->withInput();
+        }
+    }
+}
