adcfe6b2e7968fbdde91b44e3726b28c2cd9ccd5
IEBrosalin <iebrosalin@gmail.com>
2020-01-29 13:02:30 +0700

    update sql dump for fixture

--- app/propel/sql/news.sql
+++ app/propel/sql/news.sql
@@ -1,24 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 29 2020 г., 08:16
+-- Версия сервера: 8.0.15
+-- Версия PHP: 7.3.2
 
-# This is a fix for InnoDB in MySQL >= 4.1.x
-# It "suspends judgement" for fkey relationships until are tables are set.
-SET FOREIGN_KEY_CHECKS = 0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- ---------------------------------------------------------------------
--- article
--- ---------------------------------------------------------------------
 
-DROP TABLE IF EXISTS `article`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `article`
-(
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(20) DEFAULT 'Default title' NOT NULL,
-    `abstract` VARCHAR(255) NOT NULL,
-    `date` DATE NOT NULL,
-    `text` VARCHAR(1000) NOT NULL,
-    `activity` TINYINT(1) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
+--
+-- База данных: `news`
+--
+CREATE DATABASE IF NOT EXISTS `news` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `news`;
 
-# This restores the fkey checks, after having unset them earlier
-SET FOREIGN_KEY_CHECKS = 1;
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `article`
+--
+
+CREATE TABLE `article` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(20) NOT NULL DEFAULT 'Default title',
+  `abstract` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `text` varchar(1000) NOT NULL,
+  `activity` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
